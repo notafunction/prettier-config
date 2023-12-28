7ec5d62cc8eb4158b0b629c11ec3764f89952be7
Matt Latham <github@mattlatham.dev>
2023-12-27 23:24:42 -0800

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+module.exports = {
+    semi: false,
+    singleQuote: true,
+    endOfLine: 'auto',
+    printWidth: 80,
+    tabWidth: 4,
+    useTabs: false,
+    trailingComma: 'all',
+    arrowParens: 'always',
+    bracketSpacing: true,
+    quoteProps: 'as-needed',
+}
